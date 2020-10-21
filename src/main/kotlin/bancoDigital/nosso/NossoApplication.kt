289948e67eb596cb275584f621d6de6d7e94103b
Romney Matsumoto <romney.matsumoto@gmail.com>
2020-10-20 23:52:22 -0400

    First Commit

---
+++ src/main/kotlin/bancoDigital/nosso/NossoApplication.kt
@@ -0,0 +1,11 @@
+package bancoDigital.nosso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NossoApplication
+
+fun main(args: Array<String>) {
+	runApplication<NossoApplication>(*args)
+}
---
+++ src/test/kotlin/bancoDigital/nosso/NossoApplicationTests.kt
@@ -0,0 +1,13 @@
+package bancoDigital.nosso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NossoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
